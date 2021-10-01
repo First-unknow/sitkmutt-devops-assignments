a3c85e60e74e0757d066b2c72f54e562d6fd86f4
Natthasit Panjapornpol <natthasit.first@mail.kmutt.ac.th>
2021-10-01 16:04:59 +0000

    Add 07-bookinfo

---
+++ assignments/07-bookinfo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+docker build -t ratings ../ratings/
+docker build -t reviews ../reviews/
+docker build -t details ../details/
+docker build -t productpage ../productpage/
+
+docker run --name ratings -p 8080:8080 -d ratings
+docker run --name details -p 8081:8081 -d details
+docker run --name reviews -e  'ENABLE_RATINGS=true' -e 'RATINGS_SERVICE=ratings_url' -p 8082:9080 -d reviews
+docker run --name productpage --link reviews:reviews --link ratings:ratings --link reviews:reviews --link details:details -e 'RATINGS_HOSTNAME=ratings_url' -e 'DETAILS_HOSTNAME=details_url' -e 'REVIEWS_HOSTNAME=reviews_url' -p 8083:8083 -d productpage
