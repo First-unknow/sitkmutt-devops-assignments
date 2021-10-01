9add053eac5f745f86ac26d7f785c57ac7c02584
Natthasit Panjapornpol <natthasit.first@mail.kmutt.ac.th>
2021-10-01 17:57:42 +0000

    Update env at reviews

--- assignments/07-bookinfo.sh
+++ assignments/07-bookinfo.sh
@@ -9,5 +9,5 @@ docker build -t productpage ../productpage/
 
 docker run --name ratings -p 8080:8080 -d ratings
 docker run --name details -p 8081:8081 -d details
-docker run --name reviews -p 8082:9080 -d reviews
+docker run --name reviews -e 'RATINGS_SERVICE=ratings_url' -p 8082:9080 -d reviews
 docker run --name productpage --link reviews:reviews --link ratings:ratings --link reviews:reviews --link details:details -e 'RATINGS_HOSTNAME=ratings_url' -e 'DETAILS_HOSTNAME=details_url' -e 'REVIEWS_HOSTNAME=reviews_url' -p 8083:8083 -d productpage
