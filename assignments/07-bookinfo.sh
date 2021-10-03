abacd172574f9554ed296812a90eae590b49e39a
Natthasit Panjapornpol <natthasit.first@mail.kmutt.ac.th>
2021-10-03 12:22:33 +0000

    Fixed connection error

--- assignments/07-bookinfo.sh
+++ assignments/07-bookinfo.sh
@@ -6,8 +6,18 @@ docker build -t ratings ../ratings/
 docker build -t reviews ../reviews/
 docker build -t details ../details/
 docker build -t productpage ../productpage/
-
+ 
 docker run --name ratings -p 8080:8080 -d ratings
+
+docker run -d --name mongodb -p 27017:27017 \
+  -v /home/natthasit_first/sitkmutt-devops-assignments/ratings/databases:/docker-entrypoint-initdb.d \
+  bitnami/mongodb:5.0.2-debian-10-r2
+
 docker run --name details -p 8081:8081 -d details
-docker run --name reviews -e 'RATINGS_SERVICE=ratings_url' -p 8082:9080 -d reviews
-docker run --name productpage --link reviews:reviews --link ratings:ratings --link reviews:reviews --link details:details -e 'RATINGS_HOSTNAME=ratings_url' -e 'DETAILS_HOSTNAME=details_url' -e 'REVIEWS_HOSTNAME=reviews_url' -p 8083:8083 -d productpage
+
+docker run --name reviews -e 'RATINGS_SERVICE=http://ratings:8080' -p 8082:9080 -d reviews
+
+docker run --name productpage --link reviews:reviews --link ratings:ratings \
+  --link reviews:reviews --link details:details \
+  -e 'RATINGS_HOSTNAME=http://ratings:8080' -e 'DETAILS_HOSTNAME=http://details:8081' -e 'REVIEWS_HOSTNAME=http://reviews:9080' \
+  -p 8083:8083 -d productpage
